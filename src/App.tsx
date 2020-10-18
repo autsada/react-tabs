14431a4fd77a14c8891b86f74a00d2581c332572
autsada <autsada.t@gmail.com>
2020-10-18 19:53:39 +0700

    react-firestore starter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 import './App.css'
 import Tab from './Tab'
-import { products, Product } from './data'
+import { Product, products } from './data'
 
 export type TabName = 'Clothing' | 'Shoes' | 'Watches' | 'Accessories' | 'All'
 const tabNames: TabName[] = [
@@ -15,18 +15,16 @@ const tabNames: TabName[] = [
 
 function App() {
   const [activeTab, setActiveTab] = useState<TabName>('All')
-  const [displayedProducts, setDisplayedProducts] = useState<Product[]>()
+  const [displayedProducts, setDisplayedProducts] = useState<Product[] | null>()
 
   const onSelectTab = (tab: TabName) => setActiveTab(tab)
-
-  useEffect(() => {
-    setDisplayedProducts(products)
-  }, [])
 
   useEffect(() => {
     if (activeTab === 'All') {
       setDisplayedProducts(products)
     } else {
+      if (!products) return
+
       const filteredProducts = products.filter(
         (prod) => prod.category === activeTab
       )
@@ -37,6 +35,28 @@ function App() {
 
   return (
     <div className='App'>
+      <div>
+        <form className='add-product'>
+          <label htmlFor='Title'>
+            Title
+            <input type='text' name='title' />
+          </label>
+          <label htmlFor='Description'>
+            Description
+            <input type='text' name='description' />
+          </label>
+          <label htmlFor='Image'>
+            Image
+            <input type='text' name='image' />
+          </label>
+          <label htmlFor='Category'>
+            Category
+            <input type='text' name='category' />
+          </label>
+          <button type='submit'>Add</button>
+        </form>
+      </div>
+
       <div className='tab-lists'>
         {tabNames.map((name) => (
           <Tab
@@ -50,8 +70,10 @@ function App() {
 
       <div className='tab-content'>
         {displayedProducts?.map((prod) => (
-          <div className='content'>
+          <div className='content' key={prod.id}>
             <img key={prod.id} src={prod.image} alt={prod.id} width={200} />
+            <h3>{prod.title}</h3>
+            <p>{prod.description}</p>
           </div>
         ))}
       </div>
--- src/data.ts
+++ src/data.ts
@@ -1,27 +1,41 @@
 import { TabName } from './App'
 
-export type Product = { id: string; image: string; category: TabName }
+export type Product = {
+  id: string
+  title: string
+  description: string
+  image: string
+  category: TabName
+}
 
 export const products: Product[] = [
   {
     id: '1',
+    title: 'Men Shirt',
+    description: 'Male long sleeve shirt.',
     image:
       'https://previews.123rf.com/images/nikiteev/nikiteev1811/nikiteev181100117/112047271-vector-cartoon-long-sleeve-black-and-white-checkered-men-shirt.jpg',
     category: 'Clothing',
   },
   {
     id: '2',
+    title: 'Men Shoes',
+    description: 'Male nice shoes',
     image: 'https://fh.lnwfile.com/_/fh/_raw/hw/di/rw.jpg',
     category: 'Shoes',
   },
   {
     id: '3',
+    title: 'Men Watch',
+    description: 'Male luxury watch',
     image:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSAPfEgEP7Er4TpgKYuUe_zEKP6Bqu79CGP7Pd-IphfR1JURGRK0OBvkBS8ozDTHZSqD6Y729no&usqp=CAc',
     category: 'Watches',
   },
   {
     id: '4',
+    title: 'Men Wellet',
+    description: 'Male beautifule wallet.',
     image:
       'https://c1.neweggimages.com/ProductImageCompressAll1280/AFZ4_1_201907271942756525.jpg',
     category: 'Accessories',
