57378e231f23d5c4db232895dc6aa77762ea04e3
autsada <autsada.t@gmail.com>
2020-10-19 09:09:43 +0700

    react-firestore finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react'
 
 import './App.css'
 import Tab from './Tab'
-import { Product, products } from './data'
+import { Product } from './data'
+import { firestore } from './firebase'
 
 export type TabName = 'Clothing' | 'Shoes' | 'Watches' | 'Accessories' | 'All'
 const tabNames: TabName[] = [
@@ -16,6 +17,13 @@ const tabNames: TabName[] = [
 function App() {
   const [activeTab, setActiveTab] = useState<TabName>('All')
   const [displayedProducts, setDisplayedProducts] = useState<Product[] | null>()
+  const [productData, setProductData] = useState<Omit<Product, 'id'>>({
+    title: '',
+    description: '',
+    image: '',
+    category: 'Clothing',
+  })
+  const [products, setProducts] = useState<Product[] | null>(null)
 
   const onSelectTab = (tab: TabName) => setActiveTab(tab)
 
@@ -31,27 +39,94 @@ function App() {
 
       setDisplayedProducts(filteredProducts)
     }
-  }, [activeTab])
+  }, [activeTab, products])
+
+  useEffect(() => {
+    const unsubcribe = firestore.collection('products').onSnapshot({
+      next: (snapshots) => {
+        const data = snapshots.docs.map((doc) => {
+          const product = {
+            id: doc.id,
+            ...doc.data(),
+          } as Product
+
+          return product
+        })
+
+        setProducts(data)
+      },
+      error: (error) => console.log(error),
+    })
+
+    return () => unsubcribe()
+  }, [])
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const prodData = {
+      [e.target.name]: e.target.value,
+    } as Omit<Product, 'id'>
+
+    setProductData((prev) => ({ ...prev, ...prodData }))
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    // Submit the product data to firestore
+    firestore
+      .collection('products')
+      .add(productData)
+      .then(() => {
+        console.log('The product is added.')
+        setProductData({
+          title: '',
+          description: '',
+          image: '',
+          category: 'Clothing',
+        })
+      })
+      .catch((error) => console.log(error))
+  }
 
   return (
     <div className='App'>
       <div>
-        <form className='add-product'>
+        <form className='add-product' onSubmit={handleSubmit}>
           <label htmlFor='Title'>
             Title
-            <input type='text' name='title' />
+            <input
+              type='text'
+              name='title'
+              value={productData.title}
+              onChange={handleChange}
+            />
           </label>
           <label htmlFor='Description'>
             Description
-            <input type='text' name='description' />
+            <input
+              type='text'
+              name='description'
+              value={productData.description}
+              onChange={handleChange}
+            />
           </label>
           <label htmlFor='Image'>
             Image
-            <input type='text' name='image' />
+            <input
+              type='text'
+              name='image'
+              value={productData.image}
+              onChange={handleChange}
+            />
           </label>
           <label htmlFor='Category'>
             Category
-            <input type='text' name='category' />
+            <input
+              type='text'
+              name='category'
+              value={productData.category}
+              onChange={handleChange}
+            />
           </label>
           <button type='submit'>Add</button>
         </form>
@@ -69,13 +144,17 @@ function App() {
       </div>
 
       <div className='tab-content'>
-        {displayedProducts?.map((prod) => (
-          <div className='content' key={prod.id}>
-            <img key={prod.id} src={prod.image} alt={prod.id} width={200} />
-            <h3>{prod.title}</h3>
-            <p>{prod.description}</p>
-          </div>
-        ))}
+        {!displayedProducts || displayedProducts.length === 0 ? (
+          <h2>No product.</h2>
+        ) : (
+          displayedProducts?.map((prod) => (
+            <div className='content' key={prod.id}>
+              <img key={prod.id} src={prod.image} alt={prod.id} width={200} />
+              <h3>{prod.title}</h3>
+              <p>{prod.description}</p>
+            </div>
+          ))
+        )}
       </div>
     </div>
   )
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA2bivIxXUwl8daGS6fcd8yun3LnROCX8U',
+  authDomain: 'ecommerce-6a58f.firebaseapp.com',
+  databaseURL: 'https://ecommerce-6a58f.firebaseio.com',
+  projectId: 'ecommerce-6a58f',
+  storageBucket: 'ecommerce-6a58f.appspot.com',
+  messagingSenderId: '475213768195',
+  appId: '1:475213768195:web:5ec994b70393838d1ad036',
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+export const firestore = firebase.firestore()
